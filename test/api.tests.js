7bd8a873829f7ba6e731a738a6786f405342a817
George Thomas <gthomas@brandwatch.com>
2023-01-25 14:40:27 +0000

    feat: replace requestjs with axios
    as request is a long dead package & axios is widely used in our
    js ecosystem. this pr aims to have minimal changes, so using a
    callback is still api here. futher breaking changes will replace
    this with promises

--- test/api.tests.js
+++ test/api.tests.js
@@ -1,14 +1,19 @@
 'use strict';
 var sinon = require('sinon'),
-    BrandwatchApi = require('..').BrandwatchAPI,
-    request = require('request');
+    proxyquire = require('proxyquire').noCallThru();
 
 describe('brandwatchApi', function(){
     var sandbox = sinon.sandbox.create(),
-        requestStartStub;
+        axiosStub,
+        BrandwatchApi;
 
     beforeEach(function(){
-        requestStartStub = sandbox.stub(request.Request.prototype, 'init');
+        axiosStub = sandbox.stub().returns(Promise.resolve());
+        BrandwatchApi = proxyquire('../lib/api.js', {
+            'axios': {
+                create: () => axiosStub
+            }
+        }).BrandwatchAPI;
     });
     afterEach(function(){
         sandbox.restore();
@@ -33,82 +38,50 @@ describe('brandwatchApi', function(){
             expect(brandwatchApi.patch).toBeDefined();
             expect(brandwatchApi.del).toBeDefined();
         });
-        it('passes GET method to request on get', function(done){
+        it('passes GET method to request on get', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('GET');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('GET');
         });
-        it('passes POST method to request on post', function(done){
+        it('passes POST method to request on post', function(){
             brandwatchApi.post('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('POST');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('POST');
         });
-        it('passes PUT method to request on put', function(done){
+        it('passes PUT method to request on put', function(){
             brandwatchApi.put('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('PUT');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('PUT');
         });
-        it('passes PATCH method to request on patch', function(done){
+        it('passes PATCH method to request on patch', function(){
             brandwatchApi.patch('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('PATCH');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('PATCH');
         });
-        it('passes DELETE method to request on del', function(done){
+        it('passes DELETE method to request on del', function(){
             brandwatchApi.del('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('DELETE');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('DELETE');
         });
-        it('builds URL from the apiHost and apiPort passed in at creation', function(done){
+        it('builds URL from the apiHost and apiPort passed in at creation', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].uri).toEqual('http://localhost:9999/ping');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].url).toEqual('http://localhost:9999/ping');
         });
-        it('sets Authorization header when auth passed in with options', function(done){
+        it('sets Authorization header when auth passed in with options', function(){
             brandwatchApi.get({url: '/ping', auth: 'Foo'});
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].headers.Authorization).toEqual('bearer Foo');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].headers.Authorization).toEqual('bearer Foo');
         });
-        it('uses auth from requestOptions if none passed in with options', function(done){
+        it('uses auth from requestOptions if none passed in with options', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].headers.Authorization).toEqual('bearer abcdef');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].headers.Authorization).toEqual('bearer abcdef');
         });
     });
 
@@ -129,82 +102,50 @@ describe('brandwatchApi', function(){
             expect(brandwatchApi.patch).toBeDefined();
             expect(brandwatchApi.del).toBeDefined();
         });
-        it('passes GET method to request on get', function(done){
+        it('passes GET method to request on get', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('GET');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('GET');
         });
-        it('passes POST method to request on post', function(done){
+        it('passes POST method to request on post', function(){
             brandwatchApi.post('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('POST');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('POST');
         });
-        it('passes PUT method to request on put', function(done){
+        it('passes PUT method to request on put', function(){
             brandwatchApi.put('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('PUT');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('PUT');
         });
-        it('passes PATCH method to request on patch', function(done){
+        it('passes PATCH method to request on patch', function(){
             brandwatchApi.patch('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('PATCH');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('PATCH');
         });
-        it('passes DELETE method to request on del', function(done){
+        it('passes DELETE method to request on del', function(){
             brandwatchApi.del('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.calledOnce).toEqual(true);
-                expect(requestStartStub.thisValues[0].method).toEqual('DELETE');
-
-                done();
-            });
+            expect(axiosStub.calledOnce).toEqual(true);
+            expect(axiosStub.args[0][0].method).toEqual('DELETE');
         });
-        it('builds URL from the apiHost and apiPort passed in at creation', function(done){
+        it('builds URL from the apiHost and apiPort passed in at creation', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].uri).toEqual('http://localhost:9999/ping');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].url).toEqual('http://localhost:9999/ping');
         });
-        it('sets Authorization header when auth passed in with options', function(done){
+        it('sets Authorization header when auth passed in with options', function(){
             brandwatchApi.get({url: '/ping', auth: 'Foo'});
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].headers.Authorization).toEqual('bearer Foo');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].headers.Authorization).toEqual('bearer Foo');
         });
-        it('uses auth from requestOptions if none passed in with options', function(done){
+        it('uses auth from requestOptions if none passed in with options', function(){
             brandwatchApi.get('/ping');
 
-            process.nextTick(function(){
-                expect(requestStartStub.args[0][0].headers.Authorization).toEqual('bearer abcdef');
-
-                done();
-            });
+            expect(axiosStub.args[0][0].headers.Authorization).toEqual('bearer abcdef');
         });
     });
 });
